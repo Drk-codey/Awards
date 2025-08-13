2cafb06b3f63d86f6681c55012188c1af82ced14
Drk~Codey <109389152+Drk-codey@users.noreply.github.com>
2025-08-13 21:15:45 +0100

    feat: Add App styling and App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Hero from './Components/Hero';
+import About from './Components/About';
+import Navbar from './Components/Navbar';
+import Features from './Components/Features';
+import Story from './Components/Story';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
  
 const App = () => {
   return (
-    <main>
-      <h1></h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   )
 }
